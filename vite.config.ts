f831584ae2c69ab90429fdb488f616887545e702
Sahil Kumar <sahilkumar952558@gmail.com>
2026-04-19 22:49:24 +0530

    vercel deployed bug-fixing

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   cloudflare: false,
   tanstackStart: {
-    server: {
-      preset: "vercel",
-    },
+    prerender: {
+      routes: ["/"],
+      crawlLinks: true,
+      enabled: true
+    }
   },
 });
